02a4f9dbcc5f820aa6875fba4b48e2091e0c7ded
o-licea <98656678+o-licea@users.noreply.github.com>
2024-10-28 21:03:02 -0500

    Add files via upload

---
+++ snakegame/snake.js
@@ -0,0 +1,23 @@
+// Code for Snake game
+// Created with the help of Kenny Yip Coding, check him out!
+// https://www.youtube.com/@KennyYipCoding
+
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+window.onload = function() {
+	board = document.getElementById("board");
+	board.height = rows * blockSize;
+	board.width = cols * blockSize;
+	context = board.getContext("2d");
+	
+	update();
+}
+
+function update() {
+	context.fillStyle="black";
+	context.filllRect(0, 0, board.width, board.height);
+}
